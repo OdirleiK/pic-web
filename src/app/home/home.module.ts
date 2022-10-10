83bfe6bbc4a9af08838d77420b81e5df2a8e68a4
Odirlei Oliver Kempfer Siqueira Junior <odirlei67777@gmail.com>
2022-10-10 09:58:01 -0300

    criando logindo e validação do form

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from "@angular/core";
 import { SignInComponent } from "./signin/signin.component";
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { VMessageModule } from "../shared/components/vmessages/vmessage.module";
 import { RouterModule } from "@angular/router";
+import { SignUpComponente } from "./signup/signup.component";
+
 
 @NgModule({
-    declarations: [SignInComponent],
+    declarations: [
+        SignInComponent,
+        SignUpComponente
+    ],
     imports: [
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         VMessageModule,
-        RouterModule
+        RouterModule,        
     ]
 })
 export class HomeModule{}
