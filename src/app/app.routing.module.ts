c1a3a41adbd22a58bfc880e733c0a350d8931e69
Odirlei Oliver Kempfer Siqueira Junior <odirlei67777@gmail.com>
2022-10-11 09:49:43 -0300

    adicionando comentarios e remocao de foto

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from './core/auth/auth.guard';
 
 
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { PhotoDetailsComponent } from './photos/photo-details/photo-details.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
@@ -29,6 +30,10 @@ const routes: Routes = [
         path: 'p/add',
          component: PhotoFormComponent,
          canActivate: [AuthGuard]
+    },
+    { 
+        path: 'p/:photoId',
+         component: PhotoDetailsComponent,
     },
     { 
         path: '**',
