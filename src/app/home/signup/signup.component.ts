8b18886420d07d5cad7468a882646893c3ae5c4f
Odirlei Oliver Kempfer Siqueira Junior <odirlei67777@gmail.com>
2022-10-13 11:00:34 -0300

    alteração em validacao e queryParams

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -6,6 +6,7 @@ import { lowerCaseValidator } from "src/app/shared/validators/lower-case.validat
 import { NewUser } from "./new-user";
 import { SignUpService } from "./signup.service";
 import { UserNotTakenValidatorService } from "./user-not-taken.validator.service";
+import { userNamePassoword } from "./username-password.validator";
 
 @Component({
     templateUrl: './signup.component.html',
@@ -52,6 +53,8 @@ export class SignUpComponente implements OnInit {
                     Validators.minLength(8),
                     Validators.maxLength(14),
                 ]]
+        }, {
+            validator: userNamePassoword
         });
 
         this.platformDetectorService.isPlatformBrowser() && 
@@ -59,12 +62,14 @@ export class SignUpComponente implements OnInit {
     }
 
     signup() {
-       const newUser = this.signupForm.getRawValue() as NewUser;
-       this.signUpService
-        .signup(newUser)
-        .subscribe(
-            () => this.router.navigate(['']),
-            err => console.log(err)
-        );
+        if(this.signupForm.valid && !this.signupForm.pending) {
+            const newUser = this.signupForm.getRawValue() as NewUser;
+            this.signUpService
+                .signup(newUser)
+                .subscribe(
+                    () => this.router.navigate(['']),
+                    err => console.log(err)
+                );
+            }
     }
 }
